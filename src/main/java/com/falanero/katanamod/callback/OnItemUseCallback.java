dc82dde01b19c476ca20eae19c33465f6b6c94b0
dkdrozdov <dkdrozdov@gmail.com>
2022-12-18 00:45:32 +0700

    Add Item use callback

---
+++ src/main/java/com/falanero/katanamod/callback/OnItemUseCallback.java
@@ -0,0 +1,25 @@
+package com.falanero.katanamod.callback;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+public interface OnItemUseCallback {
+    Event<OnItemUseCallback> ON_ITEM_USE_CALLBACK = EventFactory.createArrayBacked(OnItemUseCallback.class,
+            (listeners) -> (World world, PlayerEntity user, Hand hand) ->{
+                for (OnItemUseCallback listener : listeners) {
+                    ActionResult result = listener.use(world, user, hand);
+
+                    if(result != ActionResult.PASS) {
+                        return result;
+                    }
+                }
+
+                return ActionResult.PASS;
+            });
+
+    ActionResult use(World world, PlayerEntity user, Hand hand);
+}
